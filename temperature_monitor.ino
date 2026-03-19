cee3bb633d22a770f3408fc826f3403b599f31bf
shivam1517 <shivampaswan1515@gmail.com>
2026-03-19 17:29:33 +0530

    Integrate DHT sensor for temperature and humidity

---
+++ temperature_monitor.ino
@@ -0,0 +1,31 @@
+#include "DHT.h"
+
+#define DHTPIN 2     
+#define DHTTYPE DHT11  
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600);
+  dht.begin();
+}
+
+void loop() {
+  float temp = dht.readTemperature();
+  float hum = dht.readHumidity();
+
+  if (isnan(temp) || isnan(hum)) {
+    Serial.println("Sensor error!");
+    return;
+  }
+
+  Serial.print("Temperature: ");
+  Serial.print(temp);
+  Serial.print(" °C  ");
+
+  Serial.print("Humidity: ");
+  Serial.print(hum);
+  Serial.println(" %");
+
+  delay(2000);
+}
